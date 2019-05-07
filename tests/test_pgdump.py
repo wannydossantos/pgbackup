c3c711835949c5debec865e710d26a73c2f30c9f
Wanny dos Santos <wannydossantos@hotmail.com>
2019-05-07 20:06:59 +0000

    Implement S3 interactions

---
+++ tests/test_pgdump.py
@@ -0,0 +1,23 @@
+import pytest
+import subprocess
+
+from pgbackup import pgdump
+
+url = "postgres://bob:password@example.com:5432/db_one"
+
+def test_dump_calls_pg_dump(mocker):
+    """
+    Utilize pg_dump with the database URL
+    """
+    mocker.patch('subprocess.Popen')
+    assert pgdump.dump(url)
+    subprocess.Popen.assert_called_with(['pg_dump', url], stdout=subprocess.PIPE)
+
+
+def test_dump_handles_oserror(mocker):
+    """
+    pgdump.dump returns a reasonable error if pg_dump isn't installed.
+    """
+    mocker.patch('subprocess.Popen', side_effect=OSError("no such file"))
+    with pytest.raises(SystemExit):
+         pgdump.dump(url)
